1ec2ac005a45f4c8125873e4b5acb0530febf48b
yann planchais <yann.planchais@randstaddigital.fr>
2024-10-07 11:28:51 +0200

    commit spring env

---
+++ src/main/java/fr/yann/test/spring_mysql_docker/SpringMysqlDockerApplication.java
@@ -0,0 +1,13 @@
+package fr.yann.test.spring_mysql_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/yann/test/spring_mysql_docker/SpringMysqlDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.yann.test.spring_mysql_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
